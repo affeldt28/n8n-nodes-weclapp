25a8bf439c2712bd63c18a6d7bc9996a1429bd08
affeldt28 <code@affeldt.dev>
2026-06-26 16:48:42 +0200

    fix: referenced entities

--- nodes/Weclapp/descriptions/shared/QueryParameter/includeReferencedEntities.ts
+++ nodes/Weclapp/descriptions/shared/QueryParameter/includeReferencedEntities.ts
@@ -1,15 +1,36 @@
 import type { INodeProperties } from 'n8n-workflow';
 
 export const includeReferencedEntitiesQueryParameter: INodeProperties = {
-	// TODO: needs processing
 	displayName: 'Include Referenced Entities',
 	name: 'includeReferencedEntities',
-	type: 'string',
+	type: 'fixedCollection',
 	default: '',
+	placeholder: 'Add Referenced Entity',
+	typeOptions: {
+		multipleValues: true,
+	},
+	options: [
+		{
+			displayName: 'Referenced Entity',
+			name: 'entity',
+			values: [
+				{
+					displayName: 'Entity Name',
+					name: 'name',
+					type: 'string',
+					default: '',
+					required: true,
+					placeholder: 'e.g.: customer',
+				},
+			],
+		},
+	],
 	routing: {
-		send: {
-			type: 'query',
-			property: 'includeReferencedEntities',
+		request: {
+			qs: {
+				includeReferencedEntities:
+					'={{$parameter.includeReferencedEntities.entity.map((e) => e.name).join(",")}}',
+			},
 		},
 	},
 };
